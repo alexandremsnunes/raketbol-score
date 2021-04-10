720a2a44dddbbe0e193f158f333af3f0b35193d2
alexandremsnunes <msn.alexandre@gmail.com>
2021-04-10 18:55:54 -0300

    Pagina Home: setando jogadores e exibindo detalhes da partida

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { DataService, Message } from '../services/data.service';
+import { PlayerDTO } from '../models/player.dto';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,16 +8,54 @@ import { DataService, Message } from '../services/data.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  constructor(private data: DataService) {}
+  
+  player1 : PlayerDTO = {
+    name: "",
+    score: 0
+  }; 
 
-  refresh(ev) {
-    setTimeout(() => {
-      ev.detail.complete();
-    }, 3000);
+  player2 : PlayerDTO = {
+    name: "",
+    score: 0
+  }; 
+
+  values: any;
+  formGroup1: FormGroup;
+  formGroup2: FormGroup;
+  
+  constructor(public formBuilder: FormBuilder) {
+    this.formGroup1 = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(120)]],      
+    });
+    this.formGroup2 = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(120)]],      
+    });
+  
   }
 
-  getMessages(): Message[] {
-    return this.data.getMessages();
+
+  setPlayer1() {
+    this.setPlayer(this.formGroup1.value, '1')
+    console.log(this.player1)
+    
+  }
+
+  setPlayer2() {
+    this.setPlayer(this.formGroup2.value, '2')
+    console.log(this.player2)
+  }
+
+  setPlayer(obj : PlayerDTO, id : any){
+    if(id == '1'){
+      this.player1 = obj
+    }
+    else{
+      this.player2 = obj
+    }
+  }
+
+  start(){
+    console.log(`Jogador 1: ${this.player1.name} VS Jogador 2: ${this.player2.name}` )
   }
 
 }
---
+++ src/app/models/player.dto.ts
@@ -0,0 +1,4 @@
+export interface PlayerDTO {
+    name : string;
+    score? : any;
+}
